618b234dd619bb36e3779e792694cf6835140f53
Greg Morgan <gregm9999@gmail.com>
2018-11-19 02:56:46 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,11 +7,11 @@ def selects_all_bears_names_and_orders_in_alphabetical_order
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
-  "SELECT name, age FROM bears WHERE alive=1 ORDER by age";
+  "SELECT name, age FROM bears WHERE alive=1 ORDER BY age";
 end
 
 def selects_oldest_bear_and_returns_name_and_age
-  "SELECT name, age FROM bears ORDER by age ASC LIMIT 1";
+  "SELECT name, age FROM bears ORDER by age ASC limit 1";
 end
 
 def select_youngest_bear_and_returns_name_and_age
