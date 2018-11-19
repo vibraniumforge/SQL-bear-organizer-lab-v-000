1f48550b1fa0bdb693fcb6806aaf54ed442edad0
Greg Morgan <gregm9999@gmail.com>
2018-11-19 02:59:07 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -15,15 +15,15 @@ def selects_oldest_bear_and_returns_name_and_age
 end
 
 def select_youngest_bear_and_returns_name_and_age
-  "SELECT name, age FROM bears ORDER BY age ASC limit 1"
+  "SELECT name, age FROM bears ORDER BY age ASC limit 1";
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT COUNT color FROM bears"
+  "SELECT COUNT color FROM bears";
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "Write your SQL query here"
+  "SELECT COUNT temperament="goofy"";
 end
 
 def selects_bear_that_killed_Tim
