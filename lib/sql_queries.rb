cd233d1b75f89de5113f261b43d4dea980ff137a
Greg Morgan <gregm9999@gmail.com>
2018-11-19 03:04:06 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,11 +19,11 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT color FROM bears WHERE color";
+  "SELECT COUNT (color) FROM bears WHERE color";
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "SELECT COUNT(temperament) FROM bears WHERE temperament='goofy'";
+  "SELECT COUNT (temperament) FROM bears WHERE temperament='goofy'";
 end
 
 def selects_bear_that_killed_Tim
